0691d6118caff3464f722c91ecc2014647fcce9c
Mallikarjunp <mallik.pulluru@gmail.com>
2024-02-10 13:43:36 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -29005,10 +29005,11 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.parseInputs = parseInputs;
 const core = __importStar(__nccwpck_require__(749));    
 const vaildateScanResultsActionInput = (inputs) => {
+    core.info('No application found with name', inputs.source_repository);
     console.log("inputs.token--->",inputs.token);
     
      core.info("INPUTSSSSSS INPUTSSSSSS INPUTSSSSSS", inputs.check_run_id);
-    core.info('No application found with name', inputs.source_repository);
+    
     if (!inputs.token || !inputs.check_run_id || !inputs.source_repository) {
         return false;
     }
